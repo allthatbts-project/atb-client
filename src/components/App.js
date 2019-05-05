01d4189729307466020b783058bfcf4409fce899
holeman79 <holeman79@gmail.com>
2019-05-05 17:11:24 +0900

    초기 구조 잡는 작업

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { LoginPage, NotFoundPage } from 'pages';
+
+const App = () => {
+    return (
+        <div>
+            <Switch>
+                <Route path="/login" component={LoginPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './PageTemplate.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PageTemplate = ({children}) => (
+    <div className={cx('page-template')}>
+        <main>
+            {children}
+        </main>
+    </div>
+)
+
+export default PageTemplate;
